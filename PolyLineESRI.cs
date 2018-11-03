98619a02295c7ea80ae68e12095d778a253d9c80
nicoazel <nicolas.azel@gmail.com>
2018-11-03 17:47:44 -0400

    Working Demo for ESRI Line Component

--- PolyLineESRI.cs
+++ PolyLineESRI.cs
@@ -57,10 +57,14 @@ namespace BearGIS
             // All parameters must have the correct access type. If you want 
             // to import lists or trees of values, modify the ParamAccess flag.
             //pManager.AddPointParameter("linePoints", "lP", "points that compose the polyline organized in a tree", GH_ParamAccess.tree);
-            pManager.AddGeometryParameter("polyline", "pl", "points that compose the polyline organized in a tree", GH_ParamAccess.tree);
+            //pManager.AddGeometryParameter("polyline", "pl", "points that compose the polyline organized in a tree", GH_ParamAccess.tree);
             //pManager.AddCurveParameter("polyline", "pl", "points that compose the polyline organized in a tree", GH_ParamAccess.tree);
+            pManager.AddCurveParameter("polylineTree", "plTree", "points that compose the polyline organized in a tree", GH_ParamAccess.tree);
             pManager.AddTextParameter("fields", "f", "list of Fields for each geometry. This should not be a datatree but a simple list", GH_ParamAccess.list);
             pManager.AddGenericParameter("attributes", "a", "attributes for each geometry. this should be a dataTree matching the linePoints input, and fields indicies", GH_ParamAccess.tree);
+            pManager.AddTextParameter("filePath", "fp", "File Path for new geojson file", GH_ParamAccess.item);
+            pManager.AddBooleanParameter("writeFIle", "w", "set to true to write to file", GH_ParamAccess.item);
+            //pManager.AddFieldParameter("attributes", "a", "attributes for each geometry. this should be a dataTree matching the linePoints input, and fields indicies", GH_ParamAccess.tree);
         }
 
         /// <summary>
@@ -90,19 +94,16 @@ namespace BearGIS
         {
 
             // First, we need to retrieve all data from the input parameters.
-            // We'll start by declaring variables and assigning them starting values.
-            //Plane plane = Plane.WorldXY;
-            //int turns = 0;
-            //GH_Structure<Polyline> shapes = new GH_Structure<Polyline>();
-            //GH_Structure<GH_Curve> shapes = new GH_Structure<GH_Curve>();
-            GH_Structure<IGH_GeometricGoo> shapes = new GH_Structure<IGH_GeometricGoo>();
             List<string> fields = new List<string>();
             GH_Structure<IGH_Goo > attributes = new GH_Structure<IGH_Goo>();
-            //GH_Structure<IGH_Goo> attributes = new GH_Structure<IGH_Goo>();
-         
-            // Then we need to access the input parameters individually. 
-            // When data cannot be extracted from a parameter, we should abort this method.
-            if (!DA.GetDataTree(0, out shapes)) return;
+            GH_Structure<GH_Curve> inputCurveTree = new GH_Structure<GH_Curve>();
+
+            bool writeFile = false;
+            string filePath = "";
+            if(!DA.GetData(4, ref writeFile)) return;
+            if (!DA.GetData(3, ref filePath)) return;
+            // access the input parameter by index. 
+            if (!DA.GetDataTree(0, out inputCurveTree)) return;
             if (!DA.GetDataList(1, fields)) return;
             if (!DA.GetDataTree(2, out attributes)) return;
 
@@ -111,18 +112,15 @@ namespace BearGIS
                         //    AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Inner radius must be bigger than or equal to zero");
                         //    return;}
 
-
-            //Create working variables, not outputs or inputs but middle men
+            //Create geojson dic
             Dictionary<string, Object> geoDict = new Dictionary<string, Object>();
-            //List<string> err = new List<string>();
-
-            // We're set to create the spiral now. To keep the size of the SolveInstance() method small, 
+          
+            // We're set to create the geojson now. To keep the size of the SolveInstance() method small, 
             // The actual functionality will be in a different method:
-                        //Curve spiral = CreateSpiral(plane, radius0, radius1, turns);
+            //Curve spiral = CreateSpiral(plane, radius0, radius1, turns);
 
 
             //Basic esriJSON headder info
-            // Dictionary<string, Object> geoDict = new Dictionary<string, Object>();
             geoDict.Add("displayFieldName", " ");
 
             Dictionary<string, string> fieldAliasDic = new Dictionary<string, string>();
@@ -137,7 +135,7 @@ namespace BearGIS
             Dictionary<string, int> sr = new Dictionary<string, int>() { { "wkid", 102729 }, { "latestWkid", 2272 } };
             geoDict.Add("spatialReference", sr);
 
-
+            // package the below in a function
             List<Dictionary<string, string>> fieldsList = new List<Dictionary<string, string>>();
 
             foreach (var item in fields.Select((Value, Index) => new { Value, Index }))
@@ -184,11 +182,11 @@ namespace BearGIS
                 }
 
                 fieldsList.Add(fieldTypeDict);
-            }
+            }//end for each fields
             geoDict.Add("fields", fieldsList);
 
+            // package the above in a function ^^^
 
-           
 
             //features: [ 
             //    {
@@ -201,101 +199,124 @@ namespace BearGIS
             //    }
             //]
 
-
+            // Start of  feature construction----------------------------------------------------
             // create feature list
             List<Object> featuresList = new List<Object>();
 
             // for every branch  (ie feature)
-            foreach (var path in shapes.Paths) {
+            foreach (GH_Path path in inputCurveTree.Paths)
+            {
+                //set branch
+                IList branch = inputCurveTree.get_Branch(path);
 
-                var branch = shapes.get_Branch(path);//.Cast<Curve>();
-                
-                //create Feature object:    -> will be added to "features:[]"
-                Dictionary<string, Object> feature = new Dictionary<string, Object>();
-
-
-                ///////// start of geometry part\\\\\\\\\\\
-                //create the geometry object
-                Dictionary<string, Object> geometry = new Dictionary<string, Object>();
-
-                //create the Geometry{Paths: List
-                List<Object> allPaths = new List<Object>();
-
-                // go through each curve in the branch
-                //foreach (GH_Curve shape in branch)
-                foreach (IGH_GeometricGoo shape in branch)
+                //create geometry key
+                Dictionary<string, object> thisGeometry = new Dictionary<string, object>();     
+                List<object> thisPaths = new List<object>();
+                // for every curve  in branch
+                foreach (GH_Curve thisGhCurve in branch)
                 {
+                    List<List<double>> thisPath = new List<List<double>>();
 
-                    NurbsCurve curveshape;
-                    shape.CastTo<NurbsCurve>(out curveshape);
-                   // err.Add(curveshape.ToString());
-                    // create a list of coords
-                    List<Object> thisPath = new List<Object>();
-                    // go through each point on curve
-                    
-                    //foreach (Point3d point in polylineshape.ToList<Point3d>())
-                    foreach (ControlPoint point in curveshape.Points)
+                    //convert to rhino curve
+                    Curve rhinoCurve = null;
+                    GH_Convert.ToCurve(thisGhCurve, ref rhinoCurve, 0);
+                    //curve to nurbes
+                    NurbsCurve thisNurbsCurve = rhinoCurve.ToNurbsCurve();
+                    //Get list of control points
+                    Rhino.Geometry.Collections.NurbsCurvePointList theseControlPoints = thisNurbsCurve.Points;
+                    //for each control point
+                    foreach (ControlPoint thisPoint in theseControlPoints)
                     {
-                        //craete list of coords
-                        List<double> xyCoord = new List<double>();
-                        xyCoord.Add(point.Location.X);
-                        xyCoord.Add(point.Location.Y);
-                        //add coords list to curve list
-                        thisPath.Add(xyCoord);
-                    }
-                    // add path coords list to all paths list
-                    allPaths.Add(thisPath);
-                }
-                //add coords to gemoetry object
-                geometry.Add("Paths", allPaths);
+                        //create coordinate list and add to path list
+                        List<double> thisCoordinate = new List<double>();
+                        thisCoordinate.Add(thisPoint.Location.X);
+                        thisCoordinate.Add(thisPoint.Location.Y);
+                        thisPath.Add(thisCoordinate);                
+                    }//end each control point
+                    
+                    //add this path or paths
+                    thisPaths.Add(thisPath);
+                }//end of each curve in branch
+                //add all paths to geometry key for this feature
+                thisGeometry.Add("Paths", thisPaths);
 
-                //add geometry obejct to feature object
-                feature.Add("geometry", geometry);
+                //creat attriabtrues key
+                Dictionary<string, object> thisAttribtues = new Dictionary<string, object>();
+                IList attributesBranch = attributes.get_Branch(path);
 
-                /////////start of attribures\\\\\\\\\\\\\\
-
-                //create the attributes object
-                Dictionary<string, Object> attrib = new Dictionary<string, Object>();
-
-                //foreach(var field in attributes.get_Branch(path))  
-
-                for (int index = 0 ; index < attributes.get_Branch(path).Count; index++)
-                //for (int index = 0; index <= fields.Count; index+= 1)
+                //foreach (var item in attributesBranch.Select((Value, Index) => new { Value, Index })) //this needs list not IList
+                foreach ( var item in attributesBranch)
+                //for (int i = 0; i < attributesBranch.Count; i++)
                 {
-                    attrib.Add(fields[index], attributes.get_Branch(path)[index]);
+                    string thisField = fields[attributesBranch.IndexOf(item)]; //fields are string
+
+                    // ---------------------this is in order add the riight type?
+                    
+                    if (item is Grasshopper.Kernel.Types.GH_Integer)
+                    {
+                        string thisAttribute = item.ToString();
+                        Convert.ChangeType(thisAttribute, typeof(int));
+                        thisAttribtues.Add(thisField, thisAttribute);
+                    }
+
+                    else if (item is Grasshopper.Kernel.Types.GH_Number) // else if (typeItem is long //|| typeItem is ulong //|| typeItem is float //|| typeItem is double //|| typeItem is decimal)
+                    {
+                        string thisAttribute = item.ToString();
+                        Convert.ChangeType(thisAttribute, typeof(double));
+                        thisAttribtues.Add(thisField, thisAttribute);
+                    }
+
+                    else if (item is Grasshopper.Kernel.Types.GH_String)
+                    {
+                        string thisAttribute = item.ToString();
+                        thisAttribtues.Add(thisField, thisAttribute);
+                    }
+
+                    else if (item is Grasshopper.Kernel.Types.GH_Time)
+                    {
+                        string thisAttribute = item.ToString();
+                        Convert.ChangeType(thisAttribute, typeof(DateTime));
+                        thisAttribtues.Add(thisField, thisAttribute);
+                    }
+
+                    else
+                    {
+                        string thisAttribute = "wasent a type"; item.ToString();
+                        thisAttribtues.Add(thisField, thisAttribute);
+                    }
+                    
+                    // ------------------------how to add value of igh_goo verbatum....
+                    //thisAttribtues.Add(thisField, thisAttribute);
                 }
 
-                //add attribures to geature
-                feature.Add("attributes", attrib);
-                /////////finaly step for this feature\\\\\\\\\\\\\\ 
 
-                //add this feature(ie branch) to the feature list 
-                featuresList.Add(feature);
-            }//end of for each branch
+                //wrap up into feature and add to feature list;
+                Dictionary<string, Object> thisFeature = new Dictionary<string, Object>();
 
+                thisFeature.Add("geometry", thisGeometry);
+                thisFeature.Add("attributes", thisAttribtues);
+                featuresList.Add(thisFeature);
+            }//end of each branch path
+
+            // end of  feature construction
 
             // finaly add features list to master object
             geoDict.Add("features", featuresList);
 
 
-
-
-
-
-
-
-
             //Produces convert dictionary to json text
-
+        
             var json = Newtonsoft.Json.JsonConvert.SerializeObject(geoDict, Newtonsoft.Json.Formatting.Indented);
-
-            //Print(json);
-
-            //DA.SetDataList(2, err);
-            
 
             // Finally assign the retults to the output parameter.
             //DA.SetData(0, spiral);
+
+            //write string to file
+            if (writeFile == true)
+            {
+                //@"D:\path.txt"
+                System.IO.File.WriteAllText(@filePath, json);
+            }
             DA.SetData(1, json);
         }
 
